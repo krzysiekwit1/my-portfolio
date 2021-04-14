843106c933e239dc1e2b935b867cba5af6437476
Krzysztof Witkowski <krzysiekwit@gmail.com>
2021-04-14 18:31:10 +0200

    Updates

---
+++ cb1608f2-7990aa6fa293329612fb.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-7990aa6fa293329612fb.js.LICENSE.txt */
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[523],{8014:function(l,c,e){"use strict";e.d(c,{UXu:function(){return t}});var t={prefix:"fas",iconName:"angle-double-left",icon:[448,512,[],"f100","M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"]}}}]);
+//# sourceMappingURL=cb1608f2-7990aa6fa293329612fb.js.map
